7fc86abb34edfb752dec1b721d019b0034e359f9
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-01 20:22:06 -0300

    Pages: Index

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import { } from '@chakra-ui/react';
+import { Header } from '../components/Header';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | Worldtrip</title>
+      </Head>
+
+
+      <Header />
+
+    </>
+  )
+}
