cc0e2abf7ad79600350ca6f61a833bb720b63359
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-03 23:10:50 -0300

    Style: theme chakra file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import Document, { Main, Head, Html, NextScript } from 'next/document';
+import Document, { Head, Html, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
     render() {
@@ -6,10 +6,9 @@ export default class MyDocument extends Document {
             <Html>
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
-                    <link rel="preconnect" href="https://fonts.gstatic.com" />
+                    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
                     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap" rel="stylesheet" />
                 </Head>
-
                 <body>
                     <Main />
                     <NextScript />
