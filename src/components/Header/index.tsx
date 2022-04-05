43fac4b325b74f2bd8ebd9f3446e8640d3514d39
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-04 23:30:27 -0300

    Component: Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import { Logo } from './Logo';
 
 export function Header() {
     return (
-        <Flex as="header">
+        <Flex bg="white" w="100%" as="header" mx="auto" px="1rem" align="center" justify="center">
             <Logo />
         </Flex>
     );
