977cd05e81767a677a00346872b841f60af097ba
Bernardomennndes <83621700+Bernardomennndes@users.noreply.github.com>
2022-04-07 22:26:33 -0300

    Page: index page finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,28 @@
 import Head from 'next/head';
+import { GetStaticProps } from 'next/types';
+
 import { Text } from '@chakra-ui/react';
+
 import { Header } from '../components/Header';
 import { Banner } from '../components/Banner';
 import { TravelTypes } from '../components/TravelTypes';
 import { Divider } from '../components/Divider';
+import { Slider } from '../components/Slider/index';
 
-export default function Home() {
+import { api } from '../services/api';
+
+type SliderContinentDataProps = {
+  slug: string;
+  title: string;
+  summary: string;
+  image: string;
+}
+
+interface HomeProps {
+  sliderContinents: SliderContinentDataProps[];
+}
+
+export default function Home({ sliderContinents }: HomeProps) {
   return (
     <>
       <Head>
@@ -30,6 +47,20 @@ export default function Home() {
         Get on the trip!<br />Choose your continent
       </Text>
 
+      <Slider continents={sliderContinents} />
+
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const sliderContinents = await api.get('sliderContinents').then(response => response.data);
+
+  return {
+    props: {
+      sliderContinents
+    },
+    revalidate: 60 * 60 * 24 // 24 hours
+  }
 }
